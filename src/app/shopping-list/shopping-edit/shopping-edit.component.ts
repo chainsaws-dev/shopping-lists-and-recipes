cb1884ebcbaf8bb3e4467b84f8b3c4b815d9eea0
Vorobiev Alexander <chainsaws-dev@outlook.com>
2020-04-26 22:04:35 +0300

    Разработал алгоритм добавления элементов

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Ingredient } from '../../shared/ingredients.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @Input() SelectedItem: Ingredient;
+  @Input() ItemsCount: number;
+  @Output() OnAddNewItem = new EventEmitter<Ingredient>();
+  @Output() OnDeleteSelectedItem = new EventEmitter<Ingredient>();
+  @Output() OnClearItems = new EventEmitter<void>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  AddNewItem(NameInput: HTMLInputElement, AmountInput: HTMLInputElement): void {
+    this.OnAddNewItem.emit(new Ingredient(NameInput.value, parseInt(AmountInput.value, 10)));
+  }
+
+  DeleteSelectedItem() {
+    this.OnDeleteSelectedItem.emit(this.SelectedItem);
+    this.SelectedItem = null;
+  }
+
+  ClearAllItems() {
+    this.OnClearItems.emit();
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Ingredient } from '../shared/ingredients.model';
 
 @Component({
@@ -7,6 +7,8 @@ import { Ingredient } from '../shared/ingredients.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  CurrentSelectedItem: Ingredient;
+
   ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10)
@@ -15,6 +17,26 @@ export class ShoppingListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  OnAddedNewItem(NewIngredient: Ingredient) {
+    this.ingredients.push(NewIngredient);
+  }
+
+  OnDeletedSelectedItem(IngredientToDelete: Ingredient) {
+
+    const index: number = this.ingredients.indexOf(IngredientToDelete);
+    if (index !== -1) {
+      this.ingredients.splice(index, 1);
+    }
+  }
+
+  OnClearedAll() {
+    this.ingredients = [];
+  }
+
+  OnSelectItemShopList(ingredient: Ingredient) {
+    this.CurrentSelectedItem = ingredient;
   }
 
 }
