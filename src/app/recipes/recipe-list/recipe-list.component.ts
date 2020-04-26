be7182aec93c1e9fd00b68501ab3d796d115a7d4
Vorobiev Alexander <chainsaws-dev@outlook.com>
2020-04-26 21:18:00 +0300

    Доработал вывод подробностей о рецепте

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe-model';
 
 @Component({
@@ -7,15 +7,13 @@ import { Recipe } from '../recipe-model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() RecipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Test recipe #1', 'This is for test #1', 'https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg'),
     new Recipe('Test recipe #2', 'This is for test #2', 'https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg'),
-    new Recipe('Test recipe #1', 'This is for test #1', 'https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg'),
-    new Recipe('Test recipe #2', 'This is for test #2', 'https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg'),
-    new Recipe('Test recipe #1', 'This is for test #1', 'https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg'),
-    new Recipe('Test recipe #2', 'This is for test #2', 'https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg'),
-    new Recipe('Test recipe #1', 'This is for test #1', 'https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg'),
-    new Recipe('Test recipe #2', 'This is for test #2', 'https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg')
+    new Recipe('Test recipe #3', 'This is for test #3', 'https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg'),
+    new Recipe('Test recipe #4', 'This is for test #4', 'https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg'),
+    new Recipe('Test recipe #5', 'This is for test #5', 'https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg')
   ];
 
   constructor() { }
@@ -23,5 +21,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
 
   }
-
+  OnRecipeSelected(SelectedRecipe: Recipe) {
+    this.RecipeWasSelected.emit(SelectedRecipe);
+  }
 }
