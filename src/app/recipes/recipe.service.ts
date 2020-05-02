9221d7accd6cce77945d6d0baf715e37191cf7bc
Vorobiev Alexander <chainsaws-dev@outlook.com>
2020-05-02 23:00:06 +0300

    Добавил список ингредиентов

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe-model';
+import { Ingredient } from '../shared/ingredients.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +9,22 @@ export class RecipeService {
   RecipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Test recipe #1', 'This is for test #1', 'https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg'),
-    new Recipe('Test recipe #2', 'This is for test #2', 'https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg'),
-    new Recipe('Test recipe #3', 'This is for test #3', 'https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg'),
-    new Recipe('Test recipe #4', 'This is for test #4', 'https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg'),
-    new Recipe('Test recipe #5', 'This is for test #5', 'https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg')
-  ];
+    new Recipe('Test recipe #1',
+    'This is for test #1',
+    'https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg',
+    [new Ingredient('Apple', 12), new Ingredient('Orange', 32), new Ingredient('Pineapple', 2)]),
+    new Recipe('Test recipe #2',
+    'This is for test #2',
+    'https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg',
+    [new Ingredient('Apple', 12), new Ingredient('Orange', 32), new Ingredient('Pineapple', 2)]),
+    new Recipe('Test recipe #3',
+    'This is for test #3',
+    'https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg',
+    [new Ingredient('Apple', 12), new Ingredient('Orange', 32), new Ingredient('Pineapple', 2)]),
+    new Recipe('Test recipe #4',
+    'This is for test #4',
+    'https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg',
+    [new Ingredient('Apple', 12), new Ingredient('Orange', 32), new Ingredient('Pineapple', 2)]),];
   constructor() { }
 
   GetRecipes() {
