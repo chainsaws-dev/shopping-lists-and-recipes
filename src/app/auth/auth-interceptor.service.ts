63edf21cecbaf0a6dd1c4578a89c60abd637c564
Саша Воробьёв <avorobiev@deloitte.ru>
2020-07-19 00:38:22 +0300

    Доработал перехватчик, чтобы он берег заголовки

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpParams } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
@@ -8,8 +8,10 @@ export class AuthInterceptorService implements HttpInterceptor {
 
   constructor(private auth: AuthService) { }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
     if (this.auth.CheckRegistered()) {
-      const modreq = req.clone({ params: new HttpParams().set('Auth', this.auth.GetUserToken()) });
+
+      const modreq = req.clone({headers: req.headers.set('Auth', this.auth.GetUserToken()) });
       return next.handle(modreq);
     } else {
       return next.handle(req);
