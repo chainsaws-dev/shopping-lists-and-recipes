fccb516fe8f9db174480887cf7c47a984ea7c88b
Саша Воробьёв <avorobiev@deloitte.ru>
2020-10-25 09:51:34 +0300

    Создал сервис для хранения информации о файлах

--- src/app/admin/media/media.model.ts
+++ src/app/admin/media/media.model.ts
@@ -4,6 +4,14 @@ export class FiLe {
   public Filesize: number;
   public Filetype: string;
   public FileID: string;
+
+  constructor(fname: string, fsize: number, ftype: string, flink: string, ID: number) {
+    this.Filename = fname;
+    this.Filesize = fsize;
+    this.Filetype = ftype;
+    this.FileID = flink;
+    this.ID = ID;
+  }
 }
 
 export class FilesResponse {
--- src/app/admin/media/media.service.ts
+++ src/app/admin/media/media.service.ts
@@ -1,9 +1,78 @@
 import { Injectable } from '@angular/core';
+import { FiLe } from './media.model';
+import { Subject } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MediaService {
+  FileSelected = new Subject<FiLe>();
+  FilesUpdated = new Subject<void>();
+  FilesInserted = new Subject<void>();
+  FilesDeleted = new Subject<void>();
+  FilesChanged = new Subject<FiLe>();
+
+  CurrentSelectedItem: FiLe;
+  Total: number;
+
+
+  private Files: FiLe[] = [];
 
   constructor() { }
+
+  GetFiles() {
+    return this.Files.slice();
+  }
+
+  SetFiles(newfiles: FiLe[]) {
+    this.Files = newfiles;
+    this.FilesUpdated.next();
+  }
+
+  SetPagination(Total: number, Limit: number, Offset: number) {
+    this.Total = Total;
+  }
+
+  SelectItemFilesList(f: FiLe) {
+    this.CurrentSelectedItem = f;
+    this.FileSelected.next(f);
+  }
+
+  IsCurrentSelected(user: FiLe) {
+    return this.CurrentSelectedItem === user;
+  }
+
+  GetFileById(id: number) {
+
+    if (id < this.Files.length && id > 0) {
+      return this.Files[id];
+    } else {
+      return this.Files[0];
+    }
+  }
+
+  UpdateExistingFile(f: FiLe, Index: number) {
+    this.Files[Index] = f;
+    this.FilesChanged.next(f);
+  }
+
+  AddNewUser(f: FiLe) {
+
+    const nf = new FiLe(f.Filename, f.Filesize, f.Filetype, f.FileID, f.ID);
+
+    if (this.Files.length < environment.AdminUserListPageSize) {
+      this.Files.push(nf);
+    }
+
+    this.FilesChanged.next(nf);
+    this.FilesInserted.next();
+  }
+
+  DeleteUser(Index: number) {
+    this.Files.splice(Index, 1);
+    this.FilesDeleted.next();
+  }
+
 }
+
